bf01cf45bca372aad7355ebfea51f20ff3179c18
srkapi <albertowas12@hotmail.com>
2016-12-03 12:43:41 +0100

    time access successful

--- project/js/controller/timelineCtrl.js
+++ project/js/controller/timelineCtrl.js
@@ -1,13 +1,22 @@
 
 app.controller('timelineController', function($scope, userFactory) {
-
+	$scope.accesses= [];
 	var database = firebase.database();
 
 	var userId = userFactory.getUID();
-	return firebase.database().ref('/groups/' +userId).once('value').then(function(snapshot) {
-	  var username = snapshot.val().parentEmail;
-	  console.log(username);
+	firebase.database().ref('/groups/' +"dUuoiStAEXgpJ7ReIhKohA2LHke2").orderByKey().once('value').then(function(snapshot) {
+		var childKey = snapshot.child("accesses");
+		$scope.data = childKey;
+		childKey.forEach(function(childSnapshot) {
+					var access={};
+					childSnapshot.val().datetime= new Date(childSnapshot.val().datetime);
+
+		    	$scope.accesses.push(childSnapshot.val());
+					console.log(childSnapshot.val());
+					});
+		$scope.$apply();
 	});
 	$scope.message = 'TIMELINE';
+
 
 });
